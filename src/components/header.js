6da1c211d4d82ae80bd65cf05c85fb406da5da95
Christian <chris6por@gmail.com>
2018-06-30 19:43:38 +0000

    styled card components

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ const Header = () => {
             <div className ="header">
                 <div className="header__skew"> </div>
                 <div className="header__border"></div>
-                <h1>MadLibs</h1>
+                <h1>Bottega MadLibs</h1>
                 <p> Fill out the fields below and click the generate button to see the Mad Lib story.</p>
             </div>
         );
