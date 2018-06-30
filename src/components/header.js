c74ae00760413fe34a5e36a4422233be696f5e7e
Christian <chris6por@gmail.com>
2018-06-30 18:45:41 +0000

    added header background skew

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,8 @@ const Header = () => {
     return (
             <div className ="header">
                 <div className="header__skew"> </div>
-                <h1>Bottega MadLibs</h1>
+                <div className="header__border"></div>
+                <h1>MadLibs</h1>
                 <p> Fill out the fields below and click the generate button to see the Mad Lib story.</p>
             </div>
         );
